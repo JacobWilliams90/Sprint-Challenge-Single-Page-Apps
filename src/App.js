3bf0c177e47063abbf46db1956ab488f08a0984f
Jacob Williams <JaWilliamsDev@gmail.com>
2019-09-13 20:17:36 -0400

    Completed TabNav (With Learning bits of Semantic UI -_-) imported welcome page with route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage'
+import { Route } from "react-router-dom";
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
+      <TabNav />
+      <Route exact path='/' component={WelcomePage} />
     </main>
   );
 }
